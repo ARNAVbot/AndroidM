b1e25910568921d8db0b37925ff63e033498c7e9
Arnav Agarwal <arnavagarwal@ip-192-168-0-106.ap-southeast-1.compute.internal>
2021-09-27 01:49:30 +0530

    added some more

--- app/src/main/java/com/example/testmodules/MainActivity.kt
+++ app/src/main/java/com/example/testmodules/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.testmodules
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.network.NetworkConstants.PAYMENTS_CONSTANTS
+import com.example.network.NetworkConstants.PAYMENTS_CONSTANTS_2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startPaymentsActivity()
     }
 
     fun startPaymentsActivity() {
-        val callPluginData: CallPl
+        println(PAYMENTS_CONSTANTS)
+        println(PAYMENTS_CONSTANTS_2)
     }
 }
